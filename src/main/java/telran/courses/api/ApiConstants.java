9e6beb708b58c749853464ed1e1efc5359215ba6
Yuragranovsky <yuragranovsky@gmail.com>
2022-02-27 11:50:07 +0200

    Initial code

---
+++ src/main/java/telran/courses/api/ApiConstants.java
@@ -0,0 +1,10 @@
+package telran.courses.api;
+
+public interface ApiConstants {
+int MIN_HOURS = 80;
+int MAX_HOURS = 500;
+int MIN_COST = 5000;
+int MAX_COST = 20000;
+int MIN_ID = 100000;
+int MAX_ID = 999999;
+}
